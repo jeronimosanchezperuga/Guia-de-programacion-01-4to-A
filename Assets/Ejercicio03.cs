7e98e0d0e83e8576097157f560c75d9d9c619f1c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-03-31 10:44:20 -0300

    Exercise 3 started

---
+++ Assets/Ejercicio03.cs
@@ -0,0 +1,23 @@
+﻿//Crear una variable pública que permita ingresar texto.Permitir que al ejecutar el programa
+//se muestre en la consola un mensaje de saludo que incluya el texto de la variable.Por
+//ejemplo, si en la variable se ingresa el texto “Ana”, el mensaje en consola debe decir “Hola
+//Ana”.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio03 : MonoBehaviour
+{
+    public string texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
